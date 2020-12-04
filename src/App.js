2276e30fb78258e01cc4d50b2f8730a2e74b447b
Anko Overdevest <messagetoanko@gmail.com>
2020-12-04 13:59:16 +0100

    Postlist bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PostList from "./components/PostList";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   return (
     <div className="ui container">
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,30 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
-
+import { Table } from 'react-bootstrap';
 class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
+  constructor(props){
+    super(props);
+    this.state={ count: 0}
+}
+
   renderList() {
+    
     return this.props.posts.map(post => {
       return (
-        <div className="item" key={post.id}>
-          <i className="large middle aligned icon user" />
-          <div className="content">
-            <div className="description">
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>
-            </div>
-          </div>
-        </div>
+        <tr className="item" key={post.id}>
+        <td> {this.state.count + 1}</td>
+        <td>{post.title}</td>
+        <td>{post.id}</td>
+        <td>{post.body}</td>
+      </tr>
       );
     });
   }
 
   render() {
-    return <div className="ui relaxed divided list">{this.renderList()}</div>;
+    return <div>
+    <Table responsive>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>id</th>
+        <th>Description</th>
+      </tr>
+    </thead>
+    <tbody>
+    {this.renderList()}
+    </tbody>
+  </Table>
+  </div>;
   }
 }
 
